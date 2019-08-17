3576b94e82a92646df0eb758b1c2874a317ba58f
Ryan Artecona <ryanartecona@gmail.com>
2019-08-17 12:52:26 -0500

    read & print file, extract helpers out of main()

--- ch12/src/main.rs
+++ ch12/src/main.rs
@@ -1,15 +1,32 @@
 use std::env;
+use std::fs;
 
 const CMD_NAME: &'static str = "minigrep";
 
-fn main() {
+fn main() -> Result<(), String> {
     let argv: Vec<String> = env::args().collect();
+    let (_cmd, query, filename) = parse_argv(&argv)?;
+    grep(&query, &filename);
+    Ok(())
+}
+
+fn parse_argv(argv: &Vec<String>) -> Result<(&str, &str, &str), String> {
     match argv.as_slice() {
-        [_cmd, query, filename] => println!("{:?}, file {:?}", query, filename),
+        [cmd, query, filename] => Ok((&cmd, &query, &filename)),
         _ => {
             let cmd: &str = argv.get(0).map_or(CMD_NAME, |cmd| cmd.as_str());
-            println!("Usage: {} QUERY FILENAME", cmd);
-            std::process::exit(1);
+            Err(format!("Usage: {} QUERY FILENAME", cmd))
         }
     }
+}
+
+fn grep(_query: &str, filename: &str) {
+    println!("In file {}", filename);
+
+    let file_contents = fs::read_to_string(filename)
+        .expect("Couldn't read specified file.");
+
+    println!("--- text contents: ---");
+    println!("{}", file_contents);
+    println!("----------------------");
 }
