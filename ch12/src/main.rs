0b99d06aa0c94583d3a06aa681c1ba3cb3c52c5c
Ryan Artecona <ryanartecona@gmail.com>
2019-08-17 13:17:22 -0500

    improve error handling

--- ch12/src/main.rs
+++ ch12/src/main.rs
@@ -1,32 +1,51 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 const CMD_NAME: &'static str = "minigrep";
 
-fn main() -> Result<(), String> {
-    let argv: Vec<String> = env::args().collect();
-    let (_cmd, query, filename) = parse_argv(&argv)?;
-    grep(&query, &filename);
-    Ok(())
+struct Config {
+    command: String,
+    query: String,
+    filename: String,
 }
 
-fn parse_argv(argv: &Vec<String>) -> Result<(&str, &str, &str), String> {
-    match argv.as_slice() {
-        [cmd, query, filename] => Ok((&cmd, &query, &filename)),
-        _ => {
-            let cmd: &str = argv.get(0).map_or(CMD_NAME, |cmd| cmd.as_str());
-            Err(format!("Usage: {} QUERY FILENAME", cmd))
+impl Config {
+    fn from_argv(argv: &[String]) -> Result<Config, String> {
+        match argv {
+            [command, query, filename] => Ok(Config {
+                command: command.to_string(),
+                query: query.to_string(),
+                filename: filename.to_string(),
+            }),
+            _ => {
+                let cmd: &str = argv.get(0).map_or(CMD_NAME, |cmd| cmd.as_str());
+                Err(format!("Usage: {} QUERY FILENAME", cmd))
+            }
         }
     }
 }
 
-fn grep(_query: &str, filename: &str) {
-    println!("In file {}", filename);
+fn main() {
+    let argv: Vec<String> = env::args().collect();
+    let config = Config::from_argv(&argv).unwrap_or_else(|err| {
+        println!("{}", err);
+        process::exit(1)
+    });
+    if let Err(e) = grep(&config) {
+        println!("Runtime error: {}", e);
+        process::exit(1)
+    };
+}
 
-    let file_contents = fs::read_to_string(filename)
-        .expect("Couldn't read specified file.");
+fn grep(config: &Config) -> Result<(), Box<dyn Error>> {
+    println!("In file {}", &config.filename);
+
+    let file_contents = fs::read_to_string(&config.filename)?;
 
     println!("--- text contents: ---");
     println!("{}", file_contents);
     println!("----------------------");
+    Ok(())
 }
