c323777117f774198ee9e7cd56d348259510f7ae
Ryan Artecona <ryanartecona@gmail.com>
2019-08-18 23:48:53 -0500

    hardcode a valid html page response

--- ch20/src/main.rs
+++ ch20/src/main.rs
@@ -1,10 +1,26 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established.");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer));
+
+    let hello_html = fs::read_to_string("hello.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{}", hello_html);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap()
 }
